fb8f5c6316fc732c392a97f91ef494b71c1f587e
Simon Gregory <simon@helvector.org>
2012-11-18 20:07:20 +0000

    added a place search for weather

--- lib/bbc.rb
+++ lib/bbc.rb
@@ -1,8 +1,9 @@
 require 'open-uri'
 require 'json'
 
-require 'bbc/version'
 require 'bbc/colors'
 require 'bbc/forecast'
-require 'bbc/schedule'
+require 'bbc/location'
 require 'bbc/now_next'
+require 'bbc/schedule'
+require 'bbc/version'
---
+++ lib/bbc/location.rb
@@ -0,0 +1,26 @@
+class Location
+  def initialize(term)
+    abort "Please use a longer search term" if term.length < 3
+    @url = "http://www.bbc.co.uk/locator/default/en-GB/autocomplete.json?search=" + term + "&filter=domestic&ptrt=/"
+
+    load
+  end
+
+  def load
+    begin
+     raw = open(@url, 'UserAgent' => BBC::USER_AGENT).read
+     JSON.parse(raw)
+    rescue
+      puts "Unable to download location data"
+      exit
+    end
+
+    data[1].each do |e|
+      name = e['fullName'].split(',')
+      place = light_green(name[0]+ ',') + green(name[1])
+      id = cyan e['id']
+
+      puts sprintf "%-60s %-10s", place, id
+    end
+  end
+end
