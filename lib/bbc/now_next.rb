8a2914e6bf9e6c40dd9afde6a6dcb980645f2e71
Simon Gregory <simon@helvector.org>
2012-11-24 18:26:17 +0000

    extracted the how long until time formatter to its own file

--- lib/bbc/now_next.rb
+++ lib/bbc/now_next.rb
@@ -49,23 +49,4 @@ class NowNext
   def format_channel name
     name.gsub(/^BBC | (London|England|Channel)/,'')
   end
-
-  def how_long_until start
-    a = (start-Time.now).to_i
-
-    case a
-      when -10000000..0 then "On now"
-      when 0 then 'Now!'
-      when 1 then 'A second'
-      when 2..59 then a.to_s+' secs'
-      when 60..119 then 'A min' #120 = 2 minutes
-      when 120..3540 then (a/60).to_i.to_s+' mins'
-      when 3541..7100 then 'An hour' # 3600 = 1 hour
-      when 7101..82800 then ((a+99)/3600).to_i.to_s+' hours'
-      when 82801..172000 then 'A day' # 86400 = 1 day
-      when 172001..518400 then ((a+800)/(60*60*24)).to_i.to_s+' days'
-      when 518400..1036800 then 'A week'
-      else ((a+180000)/(60*60*24*7)).to_i.to_s+' weeks'
-    end
-  end
 end
